2d5b7cbfb5044411ca1939ece4fb7776ec33ce3c
RavichandraYellenki <134284687+RavichandraYellenki@users.noreply.github.com>
2023-09-02 23:28:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+ function callMe() { 
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!")
+		},1000)
+	})
+
+}
+callMe().then((message) => {
+            document.getElementById('output').textContent = message;
+        });
